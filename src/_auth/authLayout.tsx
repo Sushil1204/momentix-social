41449f04612e053bf7d32338774d7aec1330173b
Sushil1204 <sushilspundkar@gmail.com>
2024-10-06 23:45:38 +0530

    authLayout updated

--- src/_auth/authLayout.tsx
+++ src/_auth/authLayout.tsx
@@ -10,25 +10,15 @@ const AuthLayout = () => {
         <Navigate to={"/"} />
       ) : (
         <div className="w-full flex flex-col lg:flex-row items-center justify-evenly min-h-screen p-5 lg:space-x-4">
-          <div className="flex flex-col items-center  space-y-5 lg:w-1/2">
-            <img
-              src={momentix_transparent_logo}
-              alt="logo"
-              width={250}
-              height={150}
-            />
-            <h1 className="text-lg font-semibold text-gray-800 text-center lg:text-left">
-              Connecting People
-            </h1>
+          <div className="hidden md:flex flex-col items-center  space-y-5 lg:w-1/2">
             <img
               src={login_vector}
               alt="login_vector"
-              width={300}
-              height={200}
+              //   width={300}
+              //   height={200}
               className="hidden lg:block"
             />
           </div>
-
           <div className="flex flex-col items-center space-y-5 w-full lg:w-1/2">
             <Outlet />
           </div>
