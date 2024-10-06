ea42f94d2c0bdc71c387d049dfadc5ab9efecdef
Sushil1204 <sushilspundkar@gmail.com>
2024-10-06 23:09:45 +0530

    routing setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import Login from "./_auth/forms/login";
+import Home from "./_root/pages/home";
+import AuthLayout from "./_auth/authLayout";
+import Registration from "./_auth/forms/registration";
+import RootLayout from "./_root/rootLayout";
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <main className="flex h-screen">
+      <Routes>
+        {/* Public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/registration" element={<Registration />} />
+        </Route>
+
+        {/* Private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
   );
 }
 
